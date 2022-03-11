f197dc4a80734ae11f5f4aff9dd19c8f5ee70f71
mailrahulsre <59342234+mailrahulsre@users.noreply.github.com>
2022-03-11 11:26:54 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -3,7 +3,7 @@ def call(String project) {
 
 
         
-  sh 'docker build . -t deekshithsn/devops-training:$Docker_tag'
+  sh 'docker build . -t mailrahulsre/devops-training:$Docker_tag'
 		   withCredentials([string(credentialsId: 'docker_password', variable: 'docker_password')]) {
 				    
 				  sh 'docker login -u deekshithsn -p $docker_password'
